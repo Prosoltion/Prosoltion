de9c3aa80cfda92e9a52aed7d6e46444247c82f7
Prosoltion <122374447+Prosoltion@users.noreply.github.com>
2023-01-10 22:24:02 +0700

    Add files via upload

---
+++ Pascal/1/writeandwriteln.pas
@@ -0,0 +1,15 @@
+program writeandwriteln; //judul program
+uses crt; 
+begin
+	//fungsi write ialah menuliskan sesuatu di baris yang lama
+	write('Hello');
+	write('Prosoltion');
+	//akhir dari write
+	writeln('');
+	writeln(''); //memisahkan antara write dan writeln
+	//fungsi writeln ialah menuliskan sesuatu di baris yang baru
+	writeln('Hello');
+	writeln('Prosoltion');
+	//akhir dari writeln
+	readln(); //agar program tidak langsung tertutup
+end.
